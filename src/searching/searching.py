f58f919e96fa190ed141fbc544f8cd5952d7452d
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-18 16:06:07 +0100

    Implement linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,13 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
+      break
+  else:
+    return -1   # not found
   # TO-DO: add missing code
-
-   return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -11,9 +15,6 @@ def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
 
   # TO-DO: add missing code
 
@@ -22,9 +23,17 @@ def binary_search(arr, target):
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+
 
   if len(arr) == 0:
     return -1 # array empty
+    
   # TO-DO: add missing if/else statements, recursive calls
+  
+
+
+
+lst = [1,2,3,4,5, 8,55,77,88,1000]
+
+
+print(linear_search(lst, 6))
